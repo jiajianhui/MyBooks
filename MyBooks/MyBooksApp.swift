80d4cab4306c5b092c9932c6c0bb8e337099e902
jianhui <78072865+jiajianhui@users.noreply.github.com>
2025-05-31 15:25:27 +0800

    Initial Commit

---
+++ MyBooks/MyBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBooksApp.swift
+//  MyBooks
+//
+//  Created by 贾建辉 on 2025/5/31.
+//
+
+import SwiftUI
+
+@main
+struct MyBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
